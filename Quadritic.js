7d8c9ddbf9b878b57689616230aaa304253d550a
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2024-02-05 10:30:22 +0530

    adding conditions in quadritic equations

--- Quadritic.js
+++ Quadritic.js
@@ -2,9 +2,15 @@
 // equation  x^2 - 7x + 10 = 0
 
 let a = 1;
-let b = -7;
-let c = 10;
+let b = -6;
+let c = 9;
 let d = b * b - 4 * a * c;
 let roo1 = [-b + Math.sqrt(d)] / (2 * a);
 let roo2 = [-b - Math.sqrt(d)] / (2 * a);
-console.log(roo1, " ", roo2);
+if (d < 0) {
+  console.log("this equation has no real root", roo1, " ", roo2);
+} else if (d > 0) {
+  console.log("this equation has distinct real root", roo1, " ", roo2);
+} else {
+  console.log("this equation has one repated real root", roo1, " ", roo2);
+}
